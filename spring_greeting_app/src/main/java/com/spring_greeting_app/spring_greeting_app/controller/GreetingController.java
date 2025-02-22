a06ae7874e7dce1403a7d3709eef73196c19013d
Bhumi-gayakwad <bhumigayakwad61@gmail.com>
2025-02-22 16:24:04 +0530

    [Bhumika Gayakwad] Add: greeting app to UC_1-Greeting_controller_for_different_HTTP_methods

---
+++ spring_greeting_app/src/main/java/com/spring_greeting_app/spring_greeting_app/controller/GreetingController.java
@@ -0,0 +1,37 @@
+package com.spring_greeting_app.spring_greeting_app.controller;
+
+
+import com.spring_greeting_app.spring_greeting_app.model.Greeting;
+import com.spring_greeting_app .spring_greeting_app.model.UserDTO;
+import com.spring_greeting_app.spring_greeting_app.service.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/hello")
+public class GreetingController {
+    @Autowired
+    private GreetingService greetingService;
+
+    @GetMapping("/greeting")
+    public Greeting getGreeting() {
+        String message = greetingService.getGreetingMessage();
+        return new Greeting(message);
+    }
+
+    @PostMapping("/greeting")
+    public Greeting createGreeting(@RequestBody UserDTO user) {
+        return new Greeting("Hello " + user.getFirstName() + " " + user.getLastName() + " from BridgeLabz!");
+    }
+
+    @PutMapping("/greeting")
+    public Greeting updateGreeting(@RequestBody UserDTO user) {
+        return new Greeting("Updated greeting for " + user.getFirstName() + " " + user.getLastName());
+    }
+
+    @DeleteMapping("/greeting")
+    public Greeting deleteGreeting() {
+        return new Greeting("Greeting deleted successfully.");
+    }
+}
+
